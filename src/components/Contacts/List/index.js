695ae8a7970174fe0a02c42cfc47919ed4c19d36
ayseren <ayse.eren@windowslive.com>
2021-10-04 16:43:54 +0300

    filtering

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import {useState} from 'react'
 
 function List({contacts}) {
+    const [filter, setFilter] = useState("");
+
+    const filteredText = contacts.filter((item) => {
+        //neden keys aliniyor values alinmiyor????????????????????????
+        return Object.keys(item).some((key) => {
+            //if delete curly braces, return is not need
+            return item[key].toString().toLowerCase()
+                .includes(filter.toLowerCase());
+        });
+    });
+
+    console.log("filteredText", filteredText);
+
     return (
         <div>
+            <input 
+                placeholder="Filter"
+                value={filter}
+                onChange={(e) => setFilter(e.target.value)}/>
             <ul>
                 {
-                    contacts.map((contact, index) => (
+                    filteredText.map((contact, index) => (
                         <li key={index}>{contact.fullname}</li>
                     ))
                 }
