059c4fb5b19a4d669bdda050355536768c1111ad
ayseren <ayse.eren@windowslive.com>
2021-10-04 13:34:11 +0300

    drafts are created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+import Contacts from './components/Contacts';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Deneme
+      <Contacts />
     </div>
   );
 }
---
+++ src/components/Contacts/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Form from './Form'
+import List from './List'
+
+function Contacts() {
+    return (
+        <div>
+            contacts
+            <List />
+            <Form />
+        </div>
+    )
+}
+
+export default Contacts
